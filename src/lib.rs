82d2b1878a7eb5fd02bec7a1a0cf2ff0481f85aa
Karissa McKelvey <633012+okdistribute@users.noreply.github.com>
2022-01-11 12:33:20 -0800

    spaces

--- src/lib.rs
+++ src/lib.rs
@@ -4,41 +4,41 @@ use spake2::{Ed25519Group, Identity, Password, SPAKE2};
 
 fn create_code() -> ([u8; 1], [u8; 2]) {
     return (
-		rand::thread_rng().gen::<[u8; 1]>(),
-		rand::thread_rng().gen::<[u8; 2]>()
-	)
+        rand::thread_rng().gen::<[u8; 1]>(),
+        rand::thread_rng().gen::<[u8; 2]>()
+    )
 }
 
 fn get_index(val: &u8) -> String {
-	if val < &10 {
-		return format!("00{}", val.to_string()) 
-	} 
-	if val < &100 {
-		return format!("0{}", val.to_string()) 
-	}
-	return val.to_string();
+    if val < &10 {
+        return format!("00{}", val.to_string()) 
+    } 
+    if val < &100 {
+        return format!("0{}", val.to_string()) 
+    }
+    return val.to_string();
 }
 
 fn as_number (mailbox: &[u8], password: &[u8])  -> String {
-	let str_repr = format!("{} {} {}", 
-		get_index(&mailbox[0]), 
-		get_index(&password[0]),
-		get_index(&password[1])
-	);
-	return str_repr
+    let str_repr = format!("{} {} {}", 
+        get_index(&mailbox[0]), 
+        get_index(&password[0]),
+        get_index(&password[1])
+    );
+    return str_repr
 }
 
 
 fn open (mailbox: &[u8], password: &[u8]) -> (SPAKE2<Ed25519Group>, std::vec::Vec<u8>) {
-	return SPAKE2::<Ed25519Group>::start_symmetric(
-		&Password::new(password),
-		&Identity::new(mailbox)
-	)
+    return SPAKE2::<Ed25519Group>::start_symmetric(
+        &Password::new(password),
+        &Identity::new(mailbox)
+    )
 }
 
 fn finish (s1: SPAKE2<Ed25519Group>, inbound_msg: &[u8]) -> Vec<u8> {
-	let key = s1.finish(&inbound_msg).unwrap();
-	return key
+    let key = s1.finish(&inbound_msg).unwrap();
+    return key
 }
 
 
@@ -46,25 +46,25 @@ fn finish (s1: SPAKE2<Ed25519Group>, inbound_msg: &[u8]) -> Vec<u8> {
 mod tests {
     #[test]
     fn generate_and_redeem_code() {
-		let (mailbox, password) = crate::create_code();
+        let (mailbox, password) = crate::create_code();
 
- 	    println!("{:?}", password);
-		let (state_a, msg_a) = crate::open(&mailbox, &password);
-		let (state_b, msg_b) = crate::open(&mailbox, &password);
+         println!("{:?}", password);
+        let (state_a, msg_a) = crate::open(&mailbox, &password);
+        let (state_b, msg_b) = crate::open(&mailbox, &password);
 
-		let key = crate::finish(state_a, &msg_b);
-		let key2 = crate::finish(state_b, &msg_a);
+        let key = crate::finish(state_a, &msg_b);
+        let key2 = crate::finish(state_b, &msg_a);
 
-		assert_eq!(key, key2)
+        assert_eq!(key, key2)
 
     }
 
     #[test]
-	fn representation_string() {
-		let (mailbox, password) = crate::create_code();
-		let str_repr = crate::as_number(&mailbox, &password);
+    fn representation_string() {
+        let (mailbox, password) = crate::create_code();
+        let str_repr = crate::as_number(&mailbox, &password);
 
- 	    println!("{:?}", str_repr);
-	}
+         println!("{:?}", str_repr);
+    }
 
 }
